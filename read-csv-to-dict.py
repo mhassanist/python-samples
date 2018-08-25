cf0c5baa369d7df4a2a50cae2e93d7a84c14aa9b
mhassanist <m.hassan@ogr.iu.edu.tr>
2018-08-25 17:18:42 +0300

    Update read-csv-to-dict.py

--- read-csv-to-dict.py
+++ read-csv-to-dict.py
@@ -1,3 +1,20 @@
+#The file conents
+'''
+Title,Released,Label,UK Chart Position,US Chart Position,BPI Certification,RIAA Certification
+Please Please Me,22 March 1963,Parlophone(UK),1,-,Gold,Platinum
+With the Beatles,22 November 1963,Parlophone(UK),1,-,Platinum,Gold
+Beatlemania! With the Beatles,25 November 1963,Capitol(CAN),-,-,,
+Introducing... The Beatles,10 January 1964,Vee-Jay(US),-,2,,
+Meet the Beatles!,20 January 1964,Capitol(US),-,1,,5xPlatinum
+Twist and Shout,3 February 1964,Capitol(CAN),-,-,,
+The Beatles' Second Album,10 April 1964,Capitol(US),-,1,,2xPlatinum
+The Beatles' Long Tall Sally,11 May 1964,Capitol(CAN),-,-,,
+A Hard Day's Night,26 June 1964,United Artists(US)[C],-,1,,4xPlatinum
+,10 July 1964,Parlophone(UK),1,-,Gold,
+Something New,20 July 1964,Capitol(US),-,2,,Platinum
+Beatles for Sale,4 December 1964,Parlophone(UK),1,-,Gold,Platinum
+Beatles '65,15 December 1964,Capitol(US),-,1,,3xPlatinum
+'''
 import os
 
 DATA_DIR_PATH = r"C:\MSaudi\data_wrangling"
